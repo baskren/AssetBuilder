117e01a25114cedd3a8377f05089f4f43838dd5e
Ben Askren <ben@buildcalc.com>
2020-06-05 15:24:34 -0400

    Added UWP project

--- AssetBuilder/AssemblyInfo.cs
+++ AssetBuilder/AssemblyInfo.cs
@@ -2,4 +2,4 @@ using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
 [assembly: XamlCompilation(XamlCompilationOptions.Compile)]
-[assembly: ExportFont("AssetBuilder.Resources.MaterialIcons-Regualar.ttf", Alias = "MaterialIcons")]
+[assembly: ExportFont("AssetBuilder.Resources.MaterialIcons-Regular.ttf", Alias = "MaterialIcons")]
