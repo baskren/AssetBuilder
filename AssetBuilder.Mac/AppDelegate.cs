06794a5cd3bbb949a700fbb525be691717921275
Ben Askren <ben@buildcalc.com>
2020-07-21 11:50:30 -0400

    Update Mac

--- AssetBuilder.Mac/AppDelegate.cs
+++ AssetBuilder.Mac/AppDelegate.cs
@@ -1,14 +1,56 @@
-﻿using AppKit;
+﻿using System;
+using AppKit;
 using Foundation;
 using Xamarin.Forms;
 using Xamarin.Forms.Platform.MacOS;
-
+using CoreGraphics;
 using Microsoft.AppCenter.Analytics;
 using Microsoft.AppCenter.Crashes;
 using Microsoft.AppCenter.Distribute;
 
 namespace AssetBuilder.Mac
 {
+    public class MyWindow : NSWindow
+    {
+        public MyWindow()
+        {
+        }
+
+        public MyWindow(NSCoder coder) : base(coder)
+        {
+        }
+
+        public MyWindow(CGRect contentRect, NSWindowStyle aStyle, NSBackingStore bufferingType, bool deferCreation) : base(contentRect, aStyle, bufferingType, deferCreation)
+        {
+        }
+
+        public MyWindow(CGRect contentRect, NSWindowStyle aStyle, NSBackingStore bufferingType, bool deferCreation, NSScreen screen) : base(contentRect, aStyle, bufferingType, deferCreation, screen)
+        {
+        }
+
+        protected MyWindow(NSObjectFlag t) : base(t)
+        {
+        }
+
+        protected internal MyWindow(IntPtr handle) : base(handle)
+        {
+        }
+
+        [Export("showHelp:")]
+        private void HandleShowHelp(NSObject sender)
+        {
+            System.Diagnostics.Debug.WriteLine("MyWindow");
+            NSWorkspace.SharedWorkspace.OpenUrl(new NSUrl("https://github.com/baskren/AssetBuilder/issues?q=is%3Aissue+is%3Aclosed"));
+
+        }
+
+        public override void PerformClose(NSObject sender)
+        {
+            NSApplication.SharedApplication.Terminate(sender);
+        }
+
+    }
+
     [Register("AppDelegate")]
     public class AppDelegate : FormsApplicationDelegate
     {
@@ -18,8 +60,10 @@ namespace AssetBuilder.Mac
             var style = NSWindowStyle.Closable | NSWindowStyle.Resizable | NSWindowStyle.Titled;
 
             var rect = new CoreGraphics.CGRect(200, 1000, 1024, 768);
-            window = new NSWindow(rect, style, NSBackingStore.Buffered, false);
-            window.Title = "Xamarin.Forms on Mac!"; // choose your own Title here
+            //window = new NSWindow(rect, style, NSBackingStore.Buffered, false);
+            window = new MyWindow(rect, style, NSBackingStore.Buffered, false);
+
+            window.Title = "AssetBuilder for Xamarin"; // choose your own Title here
             window.TitleVisibility = NSWindowTitleVisibility.Hidden;
         }
 
@@ -40,6 +84,7 @@ namespace AssetBuilder.Mac
         {
             // Insert code here to tear down your application
         }
+
 
     }
 }
--- AssetBuilder.Mac/ViewController.cs
+++ AssetBuilder.Mac/ViewController.cs
@@ -30,5 +30,7 @@ namespace AssetBuilder.Mac
                 // Update the view, if already loaded.
             }
         }
+
+
     }
 }
